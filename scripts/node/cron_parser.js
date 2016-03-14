9926b3552ab180bb6d7513911e006cf653efb399
Gabi Kahlon <chubi123@gmail.com>
2016-03-14 11:31:58 +0200

    update script

--- scripts/node/cron_parser.js
+++ scripts/node/cron_parser.js
@@ -12,9 +12,9 @@ try {
   expression = args[2];
   iterations = args[3];
   interval = parser.parseExpression(expression);
-
+  console.log(util.format("\nUpcoming %s execution dates are:\n"), iterations);
   for (var i = 0; i < iterations; i++) {
-    console.log("Will execute on: " + interval.next().toString());
+    console.log(util.format("%s) %s", i + 1, interval.next().toString()));
   }
 } catch (err) {
   console.log('Error: ' + err.message);
